80428e53430c8027f4275c6f0209d2e6e4ffd707
Andrew <alo42@georgetown.edu>
2020-02-13 19:00:03 -0500

    initial commit

---
+++ SortAlgoBenchmarks/BubbleSort.swift
@@ -0,0 +1,15 @@
+//
+//  BubbleSort.swift
+//  SortAlgoBenchmarks
+//
+//  Created by Andrew O'Brien on 2/13/20.
+//  Copyright © 2020 Andrew O'Brien. All rights reserved.
+//
+
+import Foundation
+
+class BubbleSort {
+    static func sort<Element: Comparable>(_ array: inout [Element]) {
+        return
+    }
+}
--- SortAlgoBenchmarksTests/SortAlgoBenchmarksTests.swift
+++ SortAlgoBenchmarksTests/SortAlgoBenchmarksTests.swift
@@ -27,7 +27,8 @@ class SortAlgoBenchmarksTests: XCTestCase {
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            var arr = [1,2,3]
+            BubbleSort.sort(&arr)
         }
     }
 
