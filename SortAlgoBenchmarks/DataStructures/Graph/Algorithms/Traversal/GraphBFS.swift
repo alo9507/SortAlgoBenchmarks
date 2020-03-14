4aea94cb2dbc32ebc1182a31b0e67c9e453d58ed
Andrew <alo42@georgetown.edu>
2020-03-14 16:31:00 -0400

    added graph dfs

---
+++ SortAlgoBenchmarks/DataStructures/Graph/Algorithms/Traversal/GraphBFS.swift
@@ -0,0 +1,10 @@
+//
+//  GraphBFS.swift
+//  SortAlgoBenchmarks
+//
+//  Created by Andrew O'Brien on 3/14/20.
+//  Copyright © 2020 Andrew O'Brien. All rights reserved.
+//
+
+import Foundation
+
---
+++ SortAlgoBenchmarks/DataStructures/Graph/Algorithms/Traversal/GraphDFS.swift
@@ -0,0 +1,37 @@
+//
+//  GraphDFS.swift
+//  SortAlgoBenchmarks
+//
+//  Created by Andrew O'Brien on 3/14/20.
+//  Copyright © 2020 Andrew O'Brien. All rights reserved.
+//
+
+// O(V + E)
+import Foundation
+
+/*
+ A Depth-first Search plunges depth first into a graph without regard for which edge it takes next until it cannot go any further
+ It then backtracks and continues
+ */
+
+func graphDfs<T>(_ graph: AdjacencyList<T>,
+            _ source: GraphVertex<T>,
+            _ augmentation: @escaping (T) -> Void) {
+    var visited: [GraphVertex<T>: Bool] = [:]
+    recurseDfs(graph, source, &visited, augmentation)
+}
+
+func recurseDfs<T>(_ graph: AdjacencyList<T>,
+                   _ source: GraphVertex<T>,
+                   _ visited: inout [GraphVertex<T>: Bool],
+                   _ augmentation: @escaping (T) -> Void) {
+    if visited[source] == true { return }
+    visited[source] = true
+    augmentation(source.element)
+    
+    let neighbors = graph.getEdges(from: source)
+    
+    for next in neighbors {
+        recurseDfs(graph, next.destination, &visited, augmentation)
+    }
+}
